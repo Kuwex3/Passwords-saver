d9bc7f907cbdc0e150f98d8e995991bab5fd4c37
Kuwex3 <vizinovskiy@gmail.com>
2026-05-09 02:04:45 +0300

    add proxy-version bot

---
+++ bot/proxy_main.py
@@ -0,0 +1,28 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters.command import Command
+from aiogram.types import Message
+from aiogram.client.session.aiohttp import AiohttpSession
+
+from bot.config import token as bot_token
+
+from bot.save_pass_handler import router as save_router
+
+import asyncio
+
+session = AiohttpSession(proxy="http://127.0.0.1:1080")
+
+bot = Bot(token=bot_token, session=session)
+dp = Dispatcher()
+
+dp.include_router(save_router)
+
+@dp.message(Command("start"))
+async def main(message: Message):
+    await message.answer("Hello")
+
+async def start():
+    print("Bot ready!")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(start())
