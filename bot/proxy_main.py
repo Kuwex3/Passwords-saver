4b9f5fbbe07ca99000612d181ff0e7b765062755
Kuwex3 <vizinovskiy@gmail.com>
2026-08-13 00:16:40 +0300

    edit proxy-runner

--- bot/proxy_main.py
+++ bot/proxy_main.py
@@ -1,9 +1,10 @@
 import asyncio
+from aiohttp import ClientSession
+from aiohttp_socks import ProxyConnector
 from aiogram import Bot, Dispatcher
 from aiogram.filters.command import Command
 from aiogram.types import Message
 from aiogram.client.session.aiohttp import AiohttpSession
-from aiohttp_socks import ProxyConnector
 
 from bot.config import token as bot_token
 from bot.save_pass_handler import router as save_router
@@ -20,12 +21,13 @@ async def main(message: Message):
 
 async def start():
     connector = ProxyConnector.from_url("socks5://127.0.0.1:1080")
-    session = AiohttpSession(connector=connector)
     
-    bot = Bot(token=bot_token, session=session)
-    
-    print("Bot ready!")
-    await dp.start_polling(bot)
+    async with ClientSession(connector=connector) as client_session:
+        session = AiohttpSession(session=client_session)
+        bot = Bot(token=bot_token, session=session)
+        
+        print("Bot ready!")
+        await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(start())
