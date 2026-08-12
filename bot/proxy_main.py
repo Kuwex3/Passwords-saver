e18a60ad6b82f7fc6a20d3831459d5e9ddc05387
Kuwex3 <vizinovskiy@gmail.com>
2026-08-13 00:15:39 +0300

    edit proxy-runner

--- bot/proxy_main.py
+++ bot/proxy_main.py
@@ -9,10 +9,6 @@ from bot.config import token as bot_token
 from bot.save_pass_handler import router as save_router
 from bot.ip_giver import router as ip_router
 
-connector = ProxyConnector.from_url("socks5://127.0.0.1:1080")
-session = AiohttpSession(connector=connector)
-
-bot = Bot(token=bot_token, session=session)
 dp = Dispatcher()
 
 dp.include_router(save_router)
@@ -23,6 +19,11 @@ async def main(message: Message):
     await message.answer("Hello")
 
 async def start():
+    connector = ProxyConnector.from_url("socks5://127.0.0.1:1080")
+    session = AiohttpSession(connector=connector)
+    
+    bot = Bot(token=bot_token, session=session)
+    
     print("Bot ready!")
     await dp.start_polling(bot)
 
