3a4203ee1fc33502f4f30c0c52ea875f0240679b
Kuwex3 <vizinovskiy@gmail.com>
2026-08-13 00:12:52 +0300

    edit proxy-runner

--- bot/proxy_main.py
+++ bot/proxy_main.py
@@ -1,16 +1,16 @@
+import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters.command import Command
 from aiogram.types import Message
 from aiogram.client.session.aiohttp import AiohttpSession
+from aiohttp_socks import ProxyConnector
 
 from bot.config import token as bot_token
-
 from bot.save_pass_handler import router as save_router
 from bot.ip_giver import router as ip_router
 
-import asyncio
-
-session = AiohttpSession(proxy="http://127.0.0.1:1080")
+connector = ProxyConnector.from_url("socks5://127.0.0.1:1080")
+session = AiohttpSession(connector=connector)
 
 bot = Bot(token=bot_token, session=session)
 dp = Dispatcher()
